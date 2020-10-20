ba9162367c9771e9151685b16944d0938117ca8d
Nicole Bilaw <ngbilaw@spectrumone.co>
2020-10-20 21:22:28 +0800

    Add my listings screen in navigator

--- app/navigation/AccountNavigator.js
+++ app/navigation/AccountNavigator.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import AccountScreen from '../screens/AccountScreen';
 import MessagesScreen from '../screens/MessagesScreen';
+import MyListingsScreen from '../screens/MyListingsScreen';
 
 const Stack = createStackNavigator();
 
 const AccountNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen name="Account" component={AccountScreen} />
+    <Stack.Screen name="MyListings" component={MyListingsScreen} />
     <Stack.Screen name="Messages" component={MessagesScreen}/>
   </Stack.Navigator>
 );
