3382d56f52ea3dd38401403277ef8648723d6492
Nicole Bilaw <ngbilaw@spectrumone.co>
2020-10-20 21:21:11 +0800

    Use local backend for dev settings

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,8 +2,8 @@ import Constants from 'expo-constants';
 
 const settings = {
   dev: {
-    // apiUrl: 'http://192.168.1.13:9000/api'
-    apiUrl: 'https://done-with-it-api.herokuapp.com/api',
+    apiUrl: 'http://192.168.8.101:9000/api',
+    // apiUrl: 'https://done-with-it-api.herokuapp.com/api',
     IOS_CLIENT_ID: '781063018754-ssu55g115v4j7i51qeafsoskfssp1g3j.apps.googleusercontent.com'
   },
   staging: {
